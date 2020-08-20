dc69eb3675fdb7a928d109a764417a5356989d4f
Taylorbelk1 <belktaylor12@gmail.com>
2020-08-20 18:56:17 -0500

    added routes to app

--- bugtracker/src/App.tsx
+++ bugtracker/src/App.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import SignUp from './components/SignUp/SignUp';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import './App.css';
+import SignUp from './components/SignUp/SignUp';
 import SignIn from './components/SignIn/SignIn';
+import Dashboard from './components/Dashboard/Dashboard';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <SignIn />
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={SignIn} />
+        <Route exact path="/login" component={SignIn} />
+        <Route exact path="/register" component={SignUp} />
+        <Route exact path="/dashboard" component={Dashboard} />
+      </div>
+    </Router>
   );
 }
 
