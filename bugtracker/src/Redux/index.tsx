73325a98cc6ba11a50453169b706c8b4955a1dc5
Taylorbelk1 <belktaylor12@gmail.com>
2020-08-25 18:10:39 -0500

    added nav and ticket table to dashboard

--- bugtracker/src/Redux/index.tsx
+++ bugtracker/src/Redux/index.tsx
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import { userReducer } from "./reducers/user_reducer";
+import { ticketReducer } from './reducers/ticket_reducer';
+import { eventReducer } from './reducers/event_reducer';
 
 const rootReducer = combineReducers({
-    user: userReducer
+    user: userReducer,
+    tickets: ticketReducer,
+    loading: eventReducer
 })
 
 export type AppState = ReturnType<typeof rootReducer>
--- bugtracker/src/components/Dashboard/Dashboard.tsx
+++ bugtracker/src/components/Dashboard/Dashboard.tsx
@@ -1,15 +1,46 @@
-import React from "react";
-import { connect } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { connect } from 'react-redux'
 import { AppState } from "../../Redux";
+import { UserState } from '../../Redux/models/user_models';
+import TicketsTable from "./TicketsTable/TicketsTable";
+import Nav from "./Navigation/Nav";
+import { getUserTickets } from '../../Redux/actions/ticket_actions';
+import { setLoadingState } from "../../Redux/actions/event_actions";
+import { LoadingState } from "../../Redux/models/event_models";
+import Loading from "../loading";
 
-const Dashboard = () => {
+interface IProps {
+    user: UserState;
+    loading: LoadingState;
+}
+
+const Dashboard = (props: IProps) => {
+    const { user } = props;
+    const [isLoading, setIsLoading] = useState(false);
+    useEffect(() => {
+        setIsLoading(true);
+        getUserTickets({username: user.user.username}, user.token);
+        setIsLoading(false);
+    }, [user])
+
     return (
-        <div>Dashboard</div>
-    )
+        <div>
+            <Nav />
+            <h1>Welcome back, {user.user.first_name}</h1>
+            { isLoading 
+                ? <Loading />
+                : <TicketsTable /> 
+            }
+        </div>
+        )
 }
 
 const mstp = (state: AppState) => ({
-    user: state.user
+    user: state.user,
+    loading: state.loading
 })
 
-export default connect(mstp)(Dashboard);
+export default connect(mstp, {
+    setLoadingState,
+    getUserTickets
+})(Dashboard);
